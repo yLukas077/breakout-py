e76b4f3766654c8e3785ecd6bd56f3ba1098acc5
Lucas Vinicius Marques Barbosa <lvmb@windows.cin.ufpe.br>
2024-07-24 17:22:17 -0300

    colors

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pygame
 
+# Inicialização do Pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+# Configurações da Tela
+screen = pygame.display.set_mode((1024, 960))  
 pygame.display.set_caption("Breakout")
+
+# Cores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+ORANGE = (255, 165, 0)
+YELLOW = (255, 255, 0)
+
